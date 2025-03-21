d2710851a03aff9e2587907656a9159400fee4bc
Mark Ma <goldffd700@gmail.com>
2025-03-21 14:40:37 +0800

    fix: ai 回應開頭會加上 user 問題

--- conversion/response/chatgpt/convert.go
+++ conversion/response/chatgpt/convert.go
@@ -8,12 +8,21 @@ import (
 )
 
 func ConvertToString(chatgpt_response *chatgpt_types.ChatGPTResponse, previous_text *typings.StringStruct, role bool) string {
-	translated_response := official_types.NewChatCompletionChunk(strings.Replace(chatgpt_response.Message.Content.Parts[0].(string), previous_text.Text, "", 1))
-	if role {
-		translated_response.Choices[0].Delta.Role = chatgpt_response.Message.Author.Role
-	} else if translated_response.Choices[0].Delta.Content == "" {
-		return ""
-	}
-	previous_text.Text = chatgpt_response.Message.Content.Parts[0].(string)
-	return "data: " + translated_response.String() + "\n\n"
+    currentContent := chatgpt_response.Message.Content.Parts[0].(string)
+    
+    if role {
+        translated_response := official_types.NewChatCompletionChunk("")
+        translated_response.Choices[0].Delta.Role = chatgpt_response.Message.Author.Role
+        previous_text.Text = currentContent
+        return "data: " + translated_response.String() + "\n\n"
+    } else {
+        newContent := strings.Replace(currentContent, previous_text.Text, "", 1)
+        if newContent == "" {
+            return ""
+        }
+        
+        translated_response := official_types.NewChatCompletionChunk(newContent)
+        previous_text.Text = currentContent
+        return "data: " + translated_response.String() + "\n\n"
+    }
 }
